b71d10cc221903a22d5c99598dfeba121caf9e66
radasiaw <radasiaw@gromsocial.com>
2018-11-21 18:53:49 -0500

    Installed Parse SDK and set up login and registration view controllers

---
+++ Get Your Playlist/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  Get Your Playlist
+//
+//  Created by Radasia Waltower on 11/20/18.
+//  Copyright © 2018 Team 6. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
