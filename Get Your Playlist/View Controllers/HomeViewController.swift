eb47036d508424f60b25a2e250cd797d78d0e75c
Q.Nally <qnunnally1@student.gsu.edu>
2018-11-26 18:10:40 -0500

    Added image, font and background assets. Also the Sign Up, Registration, and Home pages have been styled. Minor details are to follow in next commit.

--- Get Your Playlist/View Controllers/HomeViewController.swift
+++ Get Your Playlist/View Controllers/HomeViewController.swift
@@ -15,70 +15,31 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var playlistsTable: UITableView!
     
     var userPlaylists: [String] = []
-    // testing playlist cells
-    var playlists: [Playlist] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        let currentUser = PFUser.current()
-//
-//        let query = PFQuery(className: "user_has_playlists")
-//        query.whereKey("user_id", equalTo: currentUser!)
-//        query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in
-//            if let error = error {
-//
-//                print(error.localizedDescription)
-//            } else if let objects = objects {
-//
-//                print("Successfully retrieved user playlists.")
-//
-//                for object in objects {
-//                    self.userPlaylists.append(object["name"] as! String)
-//                }
-//            }
-//
-//            self.playlistsTable.reloadData()
-//        }
-        
-        playlistsTable.delegate = self
-        playlistsTable.dataSource = self
+        let currentUser = PFUser.current()
 
-    }
-    
-    func createDummyPlaylists() -> [Playlist] {
-        var tempPlaylists: [Playlist] = []
-        
-        let p1 = Playlist(title: "Chill", playlistImage: UIImage(named: "j_cole"))
-        let p2 = Playlist(title: "Happy Go Lucky", playlistImage: UIImage(named: "beatles"))
-        
-        tempPlaylists.append(p1)
-        tempPlaylists.append(p2)
-        
-        return tempPlaylists
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-}
+        let query = PFQuery(className: "user_has_playlists")
+        query.whereKey("user_id", equalTo: currentUser!)
+        query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in
+            if let error = error {
 
-extension HomeViewController: UITableViewDataSource, UITableViewDelegate{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return playlists.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let playlist = playlists[indexPath.row]
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PlaylistCell") as! PlaylistTableViewCell
-        
-        cell.setPlaylist(playlist: playlist)
-        
-        return cell
+                print(error.localizedDescription)
+            } else if let objects = objects {
+
+                print("Successfully retrieved user playlists.")
+
+                for object in objects {
+                    self.userPlaylists.append(object["name"] as! String)
+                }
+            }
+
+            self.playlistsTable.reloadData()
+        }
+
     }
 }
 
