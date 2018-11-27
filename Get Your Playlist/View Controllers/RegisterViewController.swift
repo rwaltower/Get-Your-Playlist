1f14ff1e260aa77cd683dde60b1af566c8cab199
Q.Nally <qnunnally1@student.gsu.edu>
2018-11-26 19:21:50 -0500

    Update registration page with icons and connected buttons to variables

--- Get Your Playlist/View Controllers/RegisterViewController.swift
+++ Get Your Playlist/View Controllers/RegisterViewController.swift
@@ -25,10 +25,39 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var fields: UITextField = [txtFirstName, txtLastName, txtEmail, txtUsername, txtPassword, txtConfirmPassword]
+        
+        roundCorners(txtField: fields)
+        
+        let userImg = UIImage(named: "user")
+        addLeftIcon(txtField: txtFirstName, icon: userImg)
+        
+        let mailImg = UIImage(named: "mail")
+        addLeftIcon(txtField: txtEmail, icon: mailImg)
+        
+        let lockImg = UIImage(named: "lock")
+        addLeftIcon(txtField: txtPassword, icon: lockImg)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func addLeftIcon(txtField: UITextField, icon: UIImage)
+    {
+        let leftImageView = UIImageView(frame: CGRect(x: 20, y: 0, width: 20, height: 20))
+        leftImageView.image = icon
+        txtField.leftView = leftImageView
+        txtField.leftViewMode = .always
+    }
+    
+    func roundCorners(txtField: [UITextField])
+    {
+        for field in txtField
+        {
+            txtField.layer.cornerRadius = 15.0
+            txtField.layer.borderWidth = 2.0
+        }
     }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
